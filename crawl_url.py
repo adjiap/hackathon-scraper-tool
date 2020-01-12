94c700c6b10989a8d147bbba8184f6baa4c46c5e
adjiap <20769489+adjiap@users.noreply.github.com>
2020-01-12 10:29:00 +0100

    UPDATE: argparse method for individual year crawl UPDATE: Documentation

--- crawl_url.py
+++ crawl_url.py
@@ -1,4 +1,6 @@
-# UTF-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 Original tutorial came from https://www.youtube.com/watch?v=XQgXKtPSzUI
 
@@ -7,6 +9,7 @@ Original tutorial came from https://www.youtube.com/watch?v=XQgXKtPSzUI
 """
 import argparse
 import datetime
+import logging
 from typing import List, Iterable
 from bs4.element import ResultSet, Tag
 from bs4 import BeautifulSoup
@@ -16,29 +19,37 @@ import time
 import pandas as pd
 from collections import Counter
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, \
+    StaleElementReferenceException
 
 
-def getArgs(argv=None):
+def getArgs():
+    """This method reads the arguments passed during using the command line interface.
+
+    Returns:
+        Dictionary of arguments passed
+
+    """
     parser = argparse.ArgumentParser(description='Run Crawl Script to parse Hackathon.com website')
-    parser.add_argument('-y', '--year', default=datetime.datetime.now().year,
-                        help="Crawl for one particular year.")
-    parser.add_argument('-c', '--country', default="germany",
-                        help="Crawl for a particular country")
+    parser.add_argument('-y', '--year',
+                        help="Crawl for one particular year. Default is from current year to 5"
+                             "years before")
+    parser.add_argument('-c', '--country',
+                        help="Crawl for another particular country. Default is Germany")
     argvars = vars(parser.parse_args())
     return argvars
 
 
 def crawl_url(url: str) -> ResultSet(Tag):
-    """
-    This method uses Selenium WebDriver to run an automated Chrome Browser and crawl the page.
+    """This method uses Selenium WebDriver to run an automated Chrome Browser and crawl the page.
     This is required due to the Javascript in the Hackathon website which needs XHR requests
     to show all events in the year.
 
     Args:
-        url:
+        url: URL to be crawled
 
     Returns:
+        BeautifulSoup ResultSet with relevant Page Source to be processed
 
     """
     # Use selenium WebDriver to run an automated Chrome Browser.
@@ -62,13 +73,16 @@ def crawl_url(url: str) -> ResultSet(Tag):
         # TODO: optimize Try-Except
         try:
             driver.find_element_by_xpath(more_button_xpath)
-        except NoSuchElementException:
+        except NoSuchElementException as e:
+            logging.debug(e)
             break
-        more_button.click()
+        try:
+            more_button.click()
+        except StaleElementReferenceException as e:
+            logging.error(e)
     # Parse the read client by creating a BS4 object
     s_page = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
-    # container = s_page.findAll("div", {"class":"small-12 medium-6 large-12 column"})
     # We find the elements at the right side of the page
     container = s_page.find_all("div", {"class": ["ht-eb-card__right"]})
     # "row ht-idt-card__right__container"]})
@@ -77,8 +91,7 @@ def crawl_url(url: str) -> ResultSet(Tag):
 
 def scroll_down(driver: webdriver.Chrome) -> None:
     """A method for scrolling the page.
-    original code from https://stackoverflow.com/questions/48850974/
-    selenium-scroll-to-end-of-page-indynamically-loading-webpage/48851166
+    original code from https://stackoverflow.com/questions/48850974/selenium-scroll-to-end-of-page-indynamically-loading-webpage/48851166
 
     # HACK: Due to JavaScript issue where the More Button cannot be clicked unless seen on window
 
@@ -114,12 +127,12 @@ def parse_keywords(container: ResultSet(Tag), year: int) -> Counter:
 
     """
     keyword_list = []  # Type: str
-    final_list = [] # Type: Any
+    final_list = []  # Type: Any
     for tag in container:
         tag_link_list = tag.find_all("a", {"class": "ht-card-tag"})
         for tag_link in tag_link_list:
             keyword_list.append(tag_link.contents[0])
-    for k,v in Counter(keyword_list).items():
+    for k, v in Counter(keyword_list).items():
         final_list.append([year, k, v])
     data_frame = pd.DataFrame(final_list, columns=["Year", "Tag", "Count"])
     return data_frame
@@ -137,33 +150,49 @@ def parse_cities(container: ResultSet(Tag), year: int) -> pd.DataFrame:
 
     """
     keyword_list = []  # Type: str
-    final_list = [] # Type: Any
+    final_list = []  # Type: Any
     for tag in container:
         tag_link_list = tag.find_all("span", {"class": "ht-eb-card__location__place"})
         for tag_link in tag_link_list:
             keyword_list.append(tag_link.contents[0])
-    for k,v in Counter(keyword_list).items():
+    for k, v in Counter(keyword_list).items():
         final_list.append([year, k, v])
     data_frame = pd.DataFrame(final_list, columns=["Year", "City", "Count"])
     return data_frame
 
 
-def main():
-    if args["year"]:
-        final_year = args["year"]
+def main_crawl():
     if args["country"]:
         country = args["country"]
+    else:
+        country = "germany"
     df_cities = pd.DataFrame()
     df_keywords = pd.DataFrame()
-    for year in range(final_year - 5, final_year):
+    if args["year"]:
+        year = args["year"]
         main_url = f"https://www.hackathon.com/country/{country}/{year}"
-        container = crawl_url(main_url)
-        df_cities = df_cities.append(parse_cities(container, year), ignore_index=True)
-        df_keywords = df_keywords.append(parse_keywords(container, year), ignore_index=True)
+        try:
+            container = crawl_url(main_url)
+            df_cities = df_cities.append(parse_cities(container, year), ignore_index=True)
+            df_keywords = df_keywords.append(parse_keywords(container, year), ignore_index=True)
+        except ElementClickInterceptedException as e:
+            logging.error(e)
+            pass
+    else:
+        current_year = datetime.datetime.now().year
+        for year in range(current_year - 5, current_year):
+            main_url = f"https://www.hackathon.com/country/{country}/{year}"
+            try:
+                container = crawl_url(main_url)
+                df_cities = df_cities.append(parse_cities(container, year), ignore_index=True)
+                df_keywords = df_keywords.append(parse_keywords(container, year), ignore_index=True)
+            except ElementClickInterceptedException as e:
+                logging.error(e)
+                pass
     df_cities.to_csv(r"C:/Temp/Cities.csv")
     df_keywords.to_csv(r"C:/Temp/Keywords.csv")
 
 
 if __name__ == '__main__':
     args = getArgs()
-    main()
+    main_crawl()
